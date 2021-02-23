9913382db89d7ceec8909834a0446f66859e95e9
antgarcia12 <antgarcia014@gmail.com>
2021-02-22 18:00:57 -0700

    add onclick event

--- index.js
+++ index.js
@@ -4,26 +4,62 @@ function letsPlay(player1, player2) {
     }
 
     if (player1 === 'scissors' && player2 === 'paper') {
-            return 'player 1 wins';
+            return 'You Win!';
     }
 
     if (player1 === 'paper' && player2 === 'rock') {
-        return 'player 1 wins';
+        return 'You Win!';
     }
 
     if (player1 === 'rock' && player2 === 'scissors') {
-        return 'player 1 wins';
+        return 'You Win!';
     }
 
     else {
-        return 'player 2 wins';
+        return 'YOU LOSE! :(';
     }
 
 }
 
-console.log(letsPlay('paper', 'paper'));
+//Events and Event Listeners
+/*
+Events: any interaction the user has with a page
+click, hover, scrolling onto a particular section, doublechecking
+*/
 
+//When I click on a button, log the value of that button to the console "rock", "paper", "scissors"
 
+// function logValue(event) {
+//     console.log(event.target.value);
+// }
 
+function playGame(event) {
+    let userChoice = event.target.value;
+
+    let options = ["rock", "paper", "scissors"];
+
+    let randomIndex = Math.floor(Math.random() * options.length);
+
+    let computerChoice = options[randomIndex];
+
+    let winner = letsPlay(userChoice, computerChoice);
+
+    let userChoiceText = `The user chose ${userChoice}\n`;
+    let compChoiceText = `The computer chose ${computerChoice}\n`;
+    let finalText = userChoiceText + compChoiceText + winner;
+
+    let winnerLocation = document.querySelector(".choices");
+    winnerLocation.innerText = finalText;
+
+    console.log(winner);
+
+}
+let buttons = document.getElementsByClassName("rps");
+let button2 = document.querySelectorAll(".rps");
+// add an event listener to each button
+for (let i = 0; i < buttons.length; i++) {
+    let button = buttons[i];
+    button.onclick = playGame;
+}
 
 
